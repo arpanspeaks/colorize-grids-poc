1c827bab6ba09641665e4b48efef1bbea8422728
Arpan Sardar <arpanspeaks@gmail.com>
2022-01-14 01:08:21 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,26 +1,38 @@
-let n = 96;
-let colorsArray = Array(n).fill('white');
-const clearAll = () => {
-    colorsArray = Array(96).fill('white');
-    for(let i=0; i<96; i++) document.getElementById(`div-${i}`).style.backgroundColor = colorsArray[i];
-}
-
-let container = document.getElementById("container");
-container.addEventListener("mouseup", () => { onContainerMouseUp() });
-const onContainerMouseUp = () => {
-    if (isDragging) {
+let root = document.querySelector(":root");
+root.addEventListener("mousemove", () => { onRootMouseMove(event) });
+const onRootMouseMove = (e) => {
+    if (isDragging && (e.clientX < topLeftX || e.clientY < topLeftY || e.clientX > bottomRightX || e.clientY > bottomRightY)) {
         isDragging = false;
         applyColor();
     }
 }
 
-while (n--) {
+let container = document.getElementById("container");
+
+for(let i=0; i<96; i++) {
     div = document.createElement("div");
-    div.setAttribute('title', `div-${95 - n}`);
+    div.setAttribute('title', `div-${i}`);
     div.setAttribute("class", "div-grid-element");
-    div.setAttribute("id", `div-${95 - n}`);
+    div.setAttribute("id", `div-${i}`);
     div.setAttribute("dragable", false);
     container.appendChild(div);
+}
+
+let elements = document.getElementsByClassName("div-grid-element");
+
+for (element of elements) {
+    element.addEventListener("mousedown", () => { onGridMouseDown(event) });
+    element.addEventListener("mousemove", () => { onGridMouseMove(event) });
+    element.addEventListener("mouseup", () => { onGridMouseUp(event) });
+}
+
+container.addEventListener("mouseup", () => { onContainerMouseUp() });
+
+let colorsArray = Array(96).fill('white');
+
+const clearAll = () => {
+    colorsArray = Array(96).fill('white');
+    for(let i=0; i<96; i++) elements[i].style.backgroundColor = colorsArray[i];
 }
 
 let topLeftX, topLeftY, bottomRightX, bottomRightY;
@@ -32,40 +44,24 @@ topLeftY = Math.trunc(firstDiv.getBoundingClientRect().top);
 bottomRightX = Math.trunc(lastDiv.getBoundingClientRect().right);
 bottomRightY = Math.trunc(lastDiv.getBoundingClientRect().bottom);
 
-let root = document.querySelector(":root");
-root.addEventListener("mousemove", () => { onRootMouseMove(event) });
-const onRootMouseMove = (e) => {
-    if (isDragging && (e.clientX < topLeftX || e.clientY < topLeftY || e.clientX > bottomRightX || e.clientY > bottomRightY)) {
-        isDragging = false;
-        applyColor();
-    }
-}
-
-let elements = document.getElementsByClassName("div-grid-element");
-for (let i=0; i<96; i++) {
-    let element = elements[i];
-    element.addEventListener("mousedown", () => { onMouseDown(event, i) });
-    element.addEventListener("mousemove", () => { onMouseMove(event) });
-    element.addEventListener("mouseup", () => { onGridMouseUp(event) });
-}
-
 let isDragging = false, diagonalCheck = false, down, up, startX, startY, endX, endY, downRowId, downColId, upRowId, upColId;
-const onMouseDown = (e, index) => {
+
+const onGridMouseDown = (e) => {
     e.preventDefault();
     startX = e.clientX;
     startY = e.clientY;
     isDragging = true;
     diagonalCheck = false;
-    document.getElementById(`div-${index}`).style.backgroundColor = colorChosen;
     down = +e.target.id.split("-").pop();
+    document.getElementById(`div-${down}`).style.backgroundColor = colorChosen;
     downRowId = Math.trunc(down / 12);
     downColId = down % 12;
 }
 
-const onMouseMove = (e) => {
+const onGridMouseMove = (e) => {
     e.preventDefault();
-    let currIndex = +e.target.id.split("-").pop();
     if (isDragging) {
+        let currIndex = +e.target.id.split("-").pop();
         endX = e.clientX;
         endY = e.clientY;
         if (down !== currIndex) {
@@ -79,6 +75,22 @@ const onMouseMove = (e) => {
         }
         up = currIndex;
         liveRenderColor(down, up);
+    }
+}
+
+const onGridMouseUp = (e) => {
+    if(isDragging) {
+        e.preventDefault();
+        if(!diagonalCheck) up = +e.target.id.split("-").pop();
+        isDragging = false;
+        applyColor();
+    }
+}
+
+const onContainerMouseUp = () => {
+    if (isDragging) {
+        isDragging = false;
+        applyColor();
     }
 }
 
@@ -99,24 +111,16 @@ const diagonalAdjustments = (direction, currIndex) => {
 const liveRenderColor = (start, end) => {
     if (start > end) [start, end] = [end, start];
     for (let i = 0; i < 96; i++) {
-        let element = document.getElementById(`div-${i}`);
-        if (i >= start && i <= end && !(i % 12 < start % 12 || i % 12 > end % 12)) element.style.backgroundColor = colorChosen;
+        if (i >= start && i <= end && !(i % 12 < start % 12 || i % 12 > end % 12)) 
+            document.getElementById(`div-${i}`).style.backgroundColor = colorChosen;
         else element.style.backgroundColor = colorsArray[i];
     }
 }
 
-const onGridMouseUp = (e) => {
-    if(isDragging) {
-        e.preventDefault();
-        if(!diagonalCheck) up = +e.target.id.split("-").pop();
-        isDragging = false;
-        applyColor();
-    }
-}
-
 let colorChosen = "green";
+
 const applyColor = () => {
-    if (down > up)[down, up] = [up, down];
+    if (down > up) [down, up] = [up, down];
     for (let i = 0; i < 96; i++) {
         if (i >= down && i <= up && !(i % 12 < down % 12 || i % 12 > up % 12)) {
             colorsArray[i] = colorChosen;
@@ -127,6 +131,5 @@ const applyColor = () => {
 
 const onColorChange = () => {
     let colorPicker = document.getElementById("color");
-    let color = colorPicker.options[colorPicker.selectedIndex].text;
-    colorChosen = color;
+    colorChosen = colorPicker.options[colorPicker.selectedIndex].text;
 }
